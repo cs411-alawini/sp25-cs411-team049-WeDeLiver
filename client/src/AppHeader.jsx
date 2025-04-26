3871eeb5a5584dd4c616f657bb558170f6e31b3f
Lenny Lin <lennylin98@gmail.com>
2025-04-26 18:41:40 -0500

    make app header responsive

--- client/src/AppHeader.jsx
+++ client/src/AppHeader.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Burger, Container, Group, Text, Button, ActionIcon } from '@mantine/core';
+import { Burger, Container, Group, Text, Button, ActionIcon, Drawer, Stack } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -31,31 +31,108 @@ export function AppHeader({ userId, onLogout }) {
     }
   }, [userId]);
 
-  return (
-    <header className={classes.header}>
-      <Container fluid>
-        <div className={classes.inner}>
-          <Group gap={20} visibleFrom="sm">
-            {links.map((link) => (
-              <Link key={link.path} to={link.path} className={classes.link}>
-                {link.label}
-              </Link>
-            ))}
-          </Group>
+  const handleLinkClick = () => {
+    if (opened) {
+      toggle();
+    }
+  };
 
-          <Group gap={20} align="center">
-            {userId && (
-              <Text className={classes.userId}>
-                {userName}, {userId}
-              </Text>
-            )}
-            <ActionIcon variant="transparent" color="red" size="xs" onClick={() => { onLogout(); navigate('/'); }}>
-              <IconLogout size={16} />
-            </ActionIcon>
-            <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
-          </Group>
-        </div>
-      </Container>
-    </header>
+  const navigationLinks = (
+    <>
+      {links.map((link) => (
+        <Link
+          key={link.path}
+          to={link.path}
+          className={classes.link}
+          onClick={handleLinkClick}
+        >
+          {link.label}
+        </Link>
+      ))}
+    </>
+  );
+
+  const mobileNavigationLinks = (
+    <Stack gap="sm">
+      {links.map((link) => (
+        <Link
+          key={link.path}
+          to={link.path}
+          className={classes.link}
+          onClick={handleLinkClick}
+        >
+          {link.label}
+        </Link>
+      ))}
+    </Stack>
+  );
+
+  const userInfo = (
+    <Group gap={20} align="center">
+      {userId && (
+        <Text className={classes.userId}>
+          {userName}, {userId}
+        </Text>
+      )}
+      <ActionIcon
+        variant="transparent"
+        color="red"
+        size="xs"
+        onClick={() => {
+          onLogout();
+          navigate('/');
+          if (opened) toggle();
+        }}
+      >
+        <IconLogout size={16} />
+      </ActionIcon>
+    </Group>
+  );
+
+  const userInfoWithoutLogout = (
+    <Group gap={20} align="center">
+      {userId && (
+        <Text className={classes.userId}>
+          {userName}, {userId}
+        </Text>
+      )}
+    </Group>
+  );
+
+  return (
+    <>
+      <header className={classes.header}>
+        <Container fluid>
+          <div className={classes.inner}>
+            <Group gap={20} visibleFrom="sm">
+              {navigationLinks}
+            </Group>
+
+            <Group gap={20} align="center" justify="space-between" style={{ width: '100%' }} hiddenFrom="sm">
+              {userInfoWithoutLogout}
+              <Burger opened={opened} onClick={toggle} size="sm" />
+            </Group>
+
+            <Group gap={20} align="center" visibleFrom="sm">
+              {userInfo}
+            </Group>
+          </div>
+        </Container>
+      </header>
+
+      <Drawer
+        opened={opened}
+        onClose={toggle}
+        size="xs"
+        position="left"
+        hiddenFrom="sm"
+        withCloseButton={false}
+      >
+        <Stack gap="xl" p="md">
+          {userInfo}
+          {mobileNavigationLinks}
+        </Stack>
+      </Drawer>
+    </>
   );
 }
